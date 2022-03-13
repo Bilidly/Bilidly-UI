0ea3c7dd1ff6060f5171110e4c7ec782342d0948
bilidly_exchange <connected.addy@gmail.com>
2022-03-13 15:39:25 +0700

    Add fallback RPC to avoid wrong swap routing

--- stores/constants/constants.js
+++ stores/constants/constants.js
@@ -3,14 +3,14 @@ import * as contractsTestnet from './contractsTestnet'
 import * as contracts from './contracts'
 import * as actions from './actions'
 
-let isTestnet = process.env.NEXT_PUBLIC_CHAINID == 4002
+let isTestnet = process.env.NEXT_PUBLIC_CHAINID == 97
 
 // URLS
-let scan = 'https://ftmscan.com/'
+let scan = 'https://bscscan.com/'
 let cont = contracts
 
 if(isTestnet) {
-  scan = 'https://testnet.ftmscan.com/'
+  scan = 'https://testnet.bscscan.com/'
   cont = contractsTestnet
 }
 
@@ -21,3 +21,5 @@ export const ACTIONS = actions
 
 export const MAX_UINT256 = new BigNumber(2).pow(256).minus(1).toFixed(0)
 export const ZERO_ADDRESS = '0x0000000000000000000000000000000000000000'
+
+export const FALLBACK_RPC = 'https://rpc.ankr.com/bsc'
